905a9bbaf1a54712352e531bc93d132a5230aa18
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-12-14 18:26:18 +0800

    Adds Functionality

--- api/mapper.rb
+++ api/mapper.rb
@@ -3,20 +3,53 @@ require 'json'
 
 class Mapper
   include OpenURI
-  attr_accessor :stocks, :stock
+  class ::CannotReachURLException < Exception; end;
+
+  attr_accessor :stocks
 
   DEFAULT_URL = "http://pse.com.ph/stockMarket/home.html?method=getSecuritiesAndIndicesForPublic"
+  MAPPING = {
+    "totalVolume" => "volume",
+    "percChangeClose" => "percent_change",
+    "lastTradedPrice" => "last_trade_price",
+    "securityAlias" => "name",
+    "securitySymbol" => "symbol"
+  }
 
   def initialize
     self.stocks = open(DEFAULT_URL).read
+    setup
+  end
+
+  def setup
+    if parse
+      rehash
+      true
+    else
+      raise CannotReachURLException
+    end
   end
 
   def parse
     begin
-      stocks = JSON.parse stocks
+      self.stocks = JSON.parse(stocks).tap{|stocks| stocks.shift}
       true
     rescue
       false
     end
+  end
+
+  def rehash
+    self.stocks = stocks.map do |stock|
+      Hash[ MAPPING.map{|k,v| [v, stock[k]]} ]
+    end
+  end
+
+  def all
+    self.stocks
+  end
+
+  def find(sec_symbol)
+    self.stocks.detect{|stock| stock["symbol"] == sec_symbol}
   end
 end
--- spec/mapper_spec.rb
+++ spec/mapper_spec.rb
@@ -1,34 +1,100 @@
 require 'spec_helper'
+require 'set'
 
 describe 'Mapper' do
-  before :each do
-    VCR.use_cassette('stocks') do
-      @mapper = Mapper.new
+  context "unstubbed setup" do
+    describe "#setup" do
+      it "returns an error if url is invalid" do
+        stub_const("Mapper::DEFAULT_URL", "")
+        expect{Mapper.new}.to raise_exception
+      end
+
+      it "returns true if setup succeeds" do
+        VCR.use_cassette('stocks') do
+          Mapper.new.should be_true
+        end
+      end
     end
   end
 
-  describe "#parse" do
-    it "parses string to a hash" do
-      @mapper.stocks.should be_an_instance_of String
-      @mapper.parse
-      @mapper.stocks.should be_an_instance_of Hash
+  context "stubbed setup" do
+    before :each do
+      Mapper.any_instance.stub(:setup => 'true')
+      VCR.use_cassette('stocks') do
+        @mapper = Mapper.new
+      end
     end
 
-    it "returns true if successfully parsed"
-    it "returns false if error occured"
-  end
+    describe "#parse" do
+      it "parses string to a hash" do
+        @mapper.stocks.should be_an_instance_of String
+        @mapper.parse
+        @mapper.stocks.should be_an_instance_of Array
+      end
 
-  describe "#rehash" do
-    it "maps data into ruby-esque column names"
-  end
+      it "removes the first entry" do
+        initial = JSON.parse(@mapper.stocks).first
+        @mapper.parse
+        mapped = @mapper.stocks.first
+        initial.should_not eq(mapped)
+      end
 
-  describe "#find" do
-    it "returns json object of given stock"
-    it "returns volume, percent_change, last_trade_price, name, symbol"
-  end
+      it "returns true if successfully parsed" do
+        @mapper.parse.should be_true
+      end
 
-  describe "#all" do
-    it "returns an array of json objects with stocks"
+      it "returns false if error occured" do
+        @mapper.stocks = ""
+        @mapper.parse.should be_false
+      end
+    end
+
+    describe "#rehash" do
+      before :each do
+        @mapper.parse
+      end
+
+      it "maps data into ruby-esque column names" do
+        @mapper.rehash
+        expected = Set.new Mapper::MAPPING.values
+        in_data = Set.new @mapper.stocks.first.keys
+        expect(expected.subset? in_data).to be_true
+      end
+    end
+
+    describe "#find" do
+      before :each do
+        @mapper.parse
+        @mapper.rehash
+      end
+
+      it "returns hash object of given stock" do
+        @mapper.find("BDO").should be_an_instance_of Hash
+        @mapper.find("BDO")["symbol"].should == "BDO"
+      end
+
+      it "returns volume, percent_change, last_trade_price, name, symbol" do
+        expected = Set.new Mapper::MAPPING.values
+        in_data = Set.new @mapper.find("BDO").keys
+        expect(expected.subset? in_data).to be_true
+      end
+
+      it "returns nil when none is found" do
+        @mapper.find("LALALA").should be_nil
+      end
+    end
+
+    describe "#all" do
+      before :each do
+        @mapper.parse
+        @mapper.rehash
+      end
+
+      it "returns an array of hash objects with stocks" do
+        @mapper.all.should be_an_instance_of Array
+        @mapper.all.first.should be_an_instance_of Hash
+      end
+    end
 
   end
 end
