079035849870eaf18cad4e1580f8111353c2a400
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-12-15 21:43:24 +0800

    Adds Before clause

--- api/pisey-api.rb
+++ api/pisey-api.rb
@@ -1,23 +1,22 @@
 require 'open-uri'
 require 'json'
 
-get '/' do
-  $config.default_error_message
+before do
+  content_type :json
+  response['Access-Control-Allow-Origin'] = '*'
+end
 
+get '/' do
+  eval($config.default_error_message).to_json
 end
 
 get '/stocks' do
-  content_type :json
-  response['Access-Control-Allow-Origin'] = '*'
   @mapper = Mapper.new
 
   @mapper.all.to_json
 end
 
 get '/stocks/:id' do
-  content_type :json
-  response['Access-Control-Allow-Origin'] = '*'
-
   @mapper = Mapper.new
   (@mapper.find(params[:id].upcase) || {error: "No Such Stock"}).to_json
 end
